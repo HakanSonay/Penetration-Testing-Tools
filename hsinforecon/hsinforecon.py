ab876adf6a1dfd689172004ceda7d1c9cf79fb70
HakanSonay <hakansonay98@gmail.com>
2020-06-29 18:39:29 +0300

    ~

--- hsinforecon/hsinforecon.py
+++ hsinforecon/hsinforecon.py
@@ -3,22 +3,13 @@ import requests
 import socket
 import json
 
-if len(sys.argv) < 2:
-    print("Usage: " + sys.argv[0] + "<url>")
-    sys.exit(1)
-
-req = requests.get("https://" + sys.argv[1])
-for head in req.headers:
-    print(head+":", req.headers[head])
-
-
 gethostby_ = socket.gethostbyname(sys.argv[1])
 print("\nThe IP address of "+sys.argv[1] + "is :"+gethostby_+"\n")
 
 # ipinfo.io
 
-req_two = requests.get("https://ipinfo.io/"+gethostby_+"/json")
-resp_ = json.loads(req_two.text)
+req_ipinfo = requests.get("https://ipinfo.io/"+gethostby_+"/json")
+resp_ = json.loads(req_ipinfo.text)
 print("City: "+resp_["city"])
 print("Region: "+resp_["region"])
 print("Country: "+resp_["country"])
